3d33abe98bd6b7c04632e7c37464ce82ec4d11aa
Ayman-Yahia <81506765+Ayman-Yahia@users.noreply.github.com>
2021-05-26 20:04:06 +0300

    Delete my_environments/login_register directory

--- my_environments/login_register/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('',views.index),
-    path('new_user',views.add_new_user),
-    path('login',views.login_user)
-]
